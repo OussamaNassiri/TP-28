d8fb109cc107897128188b16927ec8b897fea4e3
OussamaNassiri <ouussamanassiri@gmail.com>
2023-12-28 16:29:53 +0100

    Preparaion l'environnement de travail

---
+++ AppSuiviFormationProfessionnelle/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+
+
+const container = document.querySelector("#root");
+const root = ReactDOM.createRoot(container);
+root.render(
+  <h1>Hello from VS code</h1>
+)
